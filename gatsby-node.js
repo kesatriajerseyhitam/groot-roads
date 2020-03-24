cd512839509a2b5d9cdae0f4ae15f22b1c53f99e
Raka <mgf.prauliyatama@gmail.com>
2020-03-24 15:30:38 +0700

    feat(tours-detail): configure gatsby-node to add pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,23 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const { data } = await graphql(`
+    query{
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  data.tours.edges.forEach(({ node: { slug } }) => {
+    createPage({
+      path: `tours/${slug}`,
+      component: path.resolve('./src/templates/tour/index.js'),
+      context: { slug },
+    });
+  });
+};
---
+++ src/templates/tour/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TourTemplate = () => (
+  <div>
+    This is from tour template
+  </div>
+);
+
+export default TourTemplate;
